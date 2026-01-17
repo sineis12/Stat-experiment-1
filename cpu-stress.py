4679c9ff5a1dbfc88978051f8ee02d0aa24f7779
sineis12 <sineiswork2023@gmail.com>
2026-01-17 16:17:31 -0500

    Changes to be committed: 	modified:   cpu-stress.py

--- cpu-stress.py
+++ cpu-stress.py
@@ -1,9 +1,23 @@
+import time
 import subprocess as sp
 
 fan_configuration = "pull"
 cpu_loads = [0, 25, 50, 75, 100]
 duration_per_load = 60
 
-for load in cpu_loads:
-    stressProcess = sp.Popen(["stress-ng", "--cpu", "4", "--cpu-load", str(load), "--timeout", str(duration_per_load)])
-    
+with open(f"{fan_configuration}.csv", "w") as f:
+    f.write("load,tempC\n")
+
+    for load in cpu_loads:
+
+        # record temp before stress test
+        temp = int(sp.run(["cat", "/sys/class/thermal/thermal_zone0/temp"], capture_output=True, text=True).stdout.strip()) / 1000
+        f.write(f"{load},{temp:.1f}\n")
+
+        # turns on stress test and waits
+        stress_process = sp.Popen(["stress-ng", "--cpu", "4", "--cpu-load", str(load), "--timeout", str(duration_per_load)])
+        stress_process.wait()
+
+        # record temp again after stress test
+        temp = int(sp.run(["cat", "/sys/class/thermal/thermal_zone0/temp"], capture_output=True, text=True).stdout.strip()) / 1000
+        f.write(f"{load},{temp:.1f}\n")
