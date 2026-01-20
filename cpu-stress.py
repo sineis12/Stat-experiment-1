d124d23452740d9d4c9c67c85fc85b9b6d4ebbc8
sineis12 <sineiswork2023@gmail.com>
2026-01-20 09:52:31 -0500

    Added a timer with to allow execution after 10 minutes of uptime on the computer

--- cpu-stress.py
+++ cpu-stress.py
@@ -1,12 +1,28 @@
 import subprocess as sp
+import time
 
 fan_configuration = "pull"
 cpu_loads = [100]
 duration_per_load = 120
 
+#functions
+def uptime_minutes():
+    with open("/proc/uptime") as f:
+        return float(f.read().split()[0]) / 60
+    
+def read_cpu_temp():
+    with open("/sys/class/thermal/thermal_zone0/temp") as f:
+        return int(f.read()) / 1000
+
+#timer
+timer_minutes = 10
+while uptime_minutes() < timer_minutes:
+    time.sleep(5)
+
+#actual experiment
 for load in cpu_loads:
     # record temp before stress test
-    temp = int(sp.run(["cat", "/sys/class/thermal/thermal_zone0/temp"], capture_output=True, text=True).stdout.strip()) / 1000
+    temp = read_cpu_temp()
     print(temp)
 
     # turns on stress test and waits
@@ -14,5 +30,5 @@ for load in cpu_loads:
     stress_process.wait()
 
     # record temp again after stress test
-    temp = int(sp.run(["cat", "/sys/class/thermal/thermal_zone0/temp"], capture_output=True, text=True).stdout.strip()) / 1000
+    temp = read_cpu_temp()
     print(temp)
